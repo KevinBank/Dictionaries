df92dd6aba646df393a215ff0ee4238015bc035b
kevin <kevinb2002@live.nl>
2020-04-03 15:54:19 +0200

    adds first version

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+
+public class Weapons : MonoBehaviour
+{
+    private Dictionary<string, int> weapon = new Dictionary<string, int>();
+    [SerializeField] private Text weaponTextUI;
+    [SerializeField] private Text ammoTextUI;
+    private string currentweapon;
+
+    private void Awake()
+    {
+        currentweapon = "Rocket Launcher";
+        weaponTextUI.text = currentweapon;
+        weapon["Rocket Launcher"] = 4;
+        weapon["Smg"] = 35;
+        weapon["Throwing knife"] = 5;
+        ammoTextUI.text = weapon[currentweapon].ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Switchweapon("Rocket Launcher");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Switchweapon("Smg");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Switchweapon("Throwing knife");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (weapon[currentweapon] <= 0)
+            {
+                weapon[currentweapon] = 0;
+                Debug.Log("Ammo is foetsie" + weapon[currentweapon]);
+            }
+            else
+            {
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        weapon[currentweapon] -= 1;
+        updateText();
+    }
+
+    void Switchweapon(string tempString)
+    {
+        currentweapon = tempString;
+        updateText();
+    }
+
+    void updateText()
+    {
+        weaponTextUI.text = currentweapon;
+        ammoTextUI.text = weapon[currentweapon].ToString();
+    }
+}
---
+++ Assets/Scripts/WeaponsEnum.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+
+public class WeaponsEnum : MonoBehaviour
+{
+    enum WeaponEnum {RocketLauncher,Smg,ThrowingKnife }
+    private Dictionary<WeaponEnum, int> weapon = new Dictionary<WeaponEnum, int>();
+    [SerializeField] private Text weaponTextUI;
+    [SerializeField] private Text ammoTextUI;
+    private WeaponEnum currentweapon;
+
+    private void Awake()
+    {
+        currentweapon = WeaponEnum.RocketLauncher;
+        weaponTextUI.text = System.Enum.GetName(typeof(WeaponEnum), currentweapon);
+        weapon[WeaponEnum.RocketLauncher] = 4;
+        weapon[WeaponEnum.Smg] = 35;
+        weapon[WeaponEnum.ThrowingKnife] = 5;
+        ammoTextUI.text = weapon[currentweapon].ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Switchweapon(WeaponEnum.RocketLauncher);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Switchweapon(WeaponEnum.Smg);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Switchweapon(WeaponEnum.ThrowingKnife);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (weapon[currentweapon] <= 0)
+            {
+                weapon[currentweapon] = 0;
+                Debug.Log("Magazine Empty" + weapon[currentweapon]);
+            }
+            else
+            {
+                Shoot();
+            }
+        }
+    }
+     
+    void Shoot()
+    {
+        weapon[currentweapon] -= 1;
+        updateText();
+    }
+
+    void Switchweapon(WeaponEnum weapon)
+    {
+        currentweapon = weapon;
+        updateText();
+    }
+
+    void updateText()
+    {
+        weaponTextUI.text = System.Enum.GetName(typeof(WeaponEnum), currentweapon);
+        ammoTextUI.text = weapon[currentweapon].ToString();
+    }
+}
